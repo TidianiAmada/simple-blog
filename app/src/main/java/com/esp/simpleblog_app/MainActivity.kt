000b40a42f8e687867c8a7cd384907db7d91750a
TidianiAmada <tidiani.amada@gmail.com>
2023-05-22 18:43:40 +0000

    Ajouter article view and logique

--- app/src/main/java/com/esp/simpleblog_app/MainActivity.kt
+++ app/src/main/java/com/esp/simpleblog_app/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.esp.simpleblog_app
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,7 +22,7 @@ class MainActivity : AppCompatActivity() {
                 "La confidence noie la douleur","1975-04-12")
             )
             database.ajouterArticle(Article("L'aventure ambigüe","Cheikh Hamidou Kane",
-                "J'ai choisi l'itineraire le plus succeptible de me perdre","1975-04-12"))
+                "J'ai choisi l'itineraire  le plus succeptible de me perdre","1975-04-12"))
             database.ajouterArticle(Article("La délivrance de l'erreur","Imam Ghazali",
                 "J'ai retrouvé l'âme de la religion dans le soufisme","1075-04-12"))
         }
@@ -25,5 +30,24 @@ class MainActivity : AppCompatActivity() {
         for (a in articles){
             Log.i("MainActivity","database article: "+ a)
         }
+        val button:FloatingActionButton =findViewById(R.id.floatingActionButton)
+        button.setOnClickListener {
+            setContentView(R.layout.new_article)
+        }
+
+        val titreEditText:EditText = findViewById(R.id.titre)
+        val auteurEditText:EditText = findViewById(R.id.auteur)
+        val contenuEditText:EditText= findViewById(R.id.contenu)
+        val ajouterButton: Button =findViewById(R.id.button)
+        ajouterButton.setOnClickListener {
+            val title=titreEditText.text.toString()
+            val auteur=auteurEditText.text.toString()
+            val contenu=contenuEditText.text.toString()
+            database.ajouterArticle(Article(title,auteur,contenu,"20/02/2022"))
+            setContentView(R.layout.activity_main)
+        }
+
+
     }
+
 }
